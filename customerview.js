c80cd5574b38974eac3856c1fa2bb4d26f0c2d4f
John Weber <johnhenryweber@gmail.com>
2019-10-22 07:59:02 -0700

    Base Assignment Done

--- customerview.js
+++ customerview.js
@@ -1,6 +1,7 @@
 var art = require("ascii-art");
 var mysql = require("mysql");
 var Table = require("cli-table");
+var inquirer = require("inquirer");
 
 var db = mysql.createConnection({
   host: "localhost",
@@ -16,28 +17,20 @@ var db = mysql.createConnection({
   database: "bamazon"
 });
 
-function executeQuery(sql, cb){
-  db.query(sql, function(err, result){
+async function executeQuery(sql, cb) {
+  db.query(sql, function (err, result) {
     if (err) throw err;
     cb(result);
   })
 }
 
-
-
-art.font('Quarks Bamazon', 'Doom', function(rendered){
-  console.log(rendered);
-  console.log("Your source for legally sourced items from across the Federation.");
-  console.log("All prices shown in Federation Credits.")
-});
-
-executeQuery('SELECT * from products', function(results){
+async function displayTable(results) {
   var table = new Table({
     head: ['Item ID', 'Prouct Name', 'Department Name', 'Price (C)', 'Quantity']
   });
   var allResults = [];
 
-  for (var i = 0; i < results.length; i++){
+  for (var i = 0; i < results.length; i++) {
     var theseResults = [];
 
     theseResults.push(results[i]["item_id"]);
@@ -48,9 +41,93 @@ executeQuery('SELECT * from products', function(results){
 
     allResults.push(theseResults);
   }
-  for (var i = 0; i < allResults.length; i++){
+  for (var i = 0; i < allResults.length; i++) {
     table.push(allResults[i]);
   }
 
-  console.log(table.toString());
-});
+  await console.log(table.toString());
+
+}
+
+function shopping() {
+  
+  // var promise1 = new Promise(function(resolve, reject) {
+  //   art.font('Quarks Bamazon', 'Doom', function (rendered) {
+  //     console.log(rendered);
+  //     console.log("Your source for legally sourced items from across the Federation.");
+  //     console.log("All prices shown in Federation Credits.")
+  //   });
+  //   executeQuery('SELECT * from products', displayTable);
+  // });
+  
+  // promise1.then(
+    
+  
+  
+
+  art.font('Quarks Bamazon', 'Doom', function (rendered) {
+    console.log(rendered);
+    console.log("Your source for legally sourced items from across the Federation.");
+    console.log("All prices shown in Federation Credits.")
+  });
+  executeQuery('SELECT * from products', displayTable);
+
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message: "What Item ID would you like to order?"
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to order?"
+      }
+    ]).then(function (answer) {
+      db.query(
+        "SELECT * FROM products WHERE ?",
+        [
+          {
+            item_id: answer.item
+          }
+        ], function (error, result) {
+          if (result.length > 0) {
+            displayTable(result);
+            if (result[0]["stock_quantity"] >= answer.quantity) {
+              var orderTotal = String(result[0]["price"] * answer.quantity);
+              var orderQuantity = answer.quantity;
+              inquirer
+                .prompt({
+                  name: "confirmPurchase",
+                  type: "list",
+                  message: "The total for your order is " + orderTotal + " credits... proceed?",
+                  choices: ["YES", "NO"]
+                })
+                .then(function (answer) {
+                  if (answer.confirmPurchase === "YES") {
+                    console.log("Great.. we will charge your account " + orderTotal + " credits and have that sent to your quarters.");
+                    db.query('UPDATE PRODUCTS SET ? WHERE ?',[
+                      {
+                        stock_quantity: result[0]["stock_quantity"] - orderQuantity
+                      },
+                      {
+                        item_id: result[0]["item_id"]
+                      }
+                    ], function(error, result){
+                      shopping();
+                    })
+                  }
+                  else if (answer.confirmPurchase === "NO") {
+                    console.log("Sorry to hear that");
+                    shopping();
+                  }
+                });
+            }
+          }
+        })
+    }));
+}
+
+
+shopping();
