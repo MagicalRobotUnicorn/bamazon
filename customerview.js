ce44858891e63ba19b815fe7eda8d65025c3a696
John Weber <johnhenryweber@gmail.com>
2019-10-22 06:14:52 -0700

    Table Views Work

--- customerview.js
+++ customerview.js
@@ -1,7 +1,56 @@
 var art = require("ascii-art");
+var mysql = require("mysql");
+var Table = require("cli-table");
 
-// art.Figlet.fontPath = 'Fonts';
+var db = mysql.createConnection({
+  host: "localhost",
 
-art.font('Bamazon', 'Doom', function(rendered){
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Pa$$w0rd",
+  database: "bamazon"
+});
+
+function executeQuery(sql, cb){
+  db.query(sql, function(err, result){
+    if (err) throw err;
+    cb(result);
+  })
+}
+
+
+
+art.font('Quarks Bamazon', 'Doom', function(rendered){
   console.log(rendered);
+  console.log("Your source for legally sourced items from across the Federation.");
+  console.log("All prices shown in Federation Credits.")
+});
+
+executeQuery('SELECT * from products', function(results){
+  var table = new Table({
+    head: ['Item ID', 'Prouct Name', 'Department Name', 'Price (C)', 'Quantity']
+  });
+  var allResults = [];
+
+  for (var i = 0; i < results.length; i++){
+    var theseResults = [];
+
+    theseResults.push(results[i]["item_id"]);
+    theseResults.push(results[i]["product_name"]);
+    theseResults.push(results[i]["department_name"]);
+    theseResults.push(results[i]["price"]);
+    theseResults.push(results[i]["stock_quantity"]);
+
+    allResults.push(theseResults);
+  }
+  for (var i = 0; i < allResults.length; i++){
+    table.push(allResults[i]);
+  }
+
+  console.log(table.toString());
 });
--- initializeDB.sql
+++ initializeDB.sql
@@ -11,3 +11,33 @@ CREATE TABLE products(
   stock_quantity INT(100) NOT NULL,
   PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Starfleet Phaser", "Weapons", "100.00", 6);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Starfleet Replicator", "General", "700.00", 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Engineering Tricorder", "Engineering", "75.50", 6);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Engineering Kit", "Engineering", "85.00", 6);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Quantum Spanner", "Engineering", "100.00", 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Standard Terminal", "General", "100.00", 12);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Medical Tricorder", "Medical", "85.00", 8);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Dermal Regenerator", "Medical", "600.00", 2);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Photon Torpedo", "Weapons", "15000.00", 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("ECHO PaPa 607 Killer Droid", "Weapons", "20000.00", 2);
